63a3e5f37dab604c45fe7f907952c22d058925ec
Alexandre Terrasa <alexandre@moz-code.org>
2015-12-03 18:26:13 -0500

    wrapper: display private and public readme files
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- wrapper/routes/index.js
+++ wrapper/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
+var md = require('node-markdown').Markdown;
+
+var bug_dir = '../bugs/bug_hello/'
+var config = JSON.parse(fs.readFileSync(bug_dir + 'bug.json', 'UTF-8'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET README page. */
+router.get('/readmes', function(req, res, next) {
+  var public = md(fs.readFileSync(bug_dir + 'PUBLIC/README.md', 'UTF-8'));
+  var private = md(fs.readFileSync(bug_dir + 'PRIVATE/README.md', 'UTF-8'));
+
+  res.render('readmes', { config: config, title: 'Readmes', public: public, private: private });
 });
 
 module.exports = router;
