cf6472f16463a1a4ea3414871d890f23aa40e92d
Alexandre Terrasa <alexandre@moz-code.org>
2015-12-04 00:37:18 -0500

    scoreboard: add team list and dome view
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- scoreboard/routes/index.js
+++ scoreboard/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var request = require('request');
+var db = require('mongoskin').db('mongodb://localhost:27017/csg_debug');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    db.collection('teams').find().toArray(function(err, items) {
+		// TODO sort teams by points
+		res.render('index', { title: 'Debug Competition', teams: items });
+	});
+});
+
+/* GET dome page. */
+router.get('/team/:id', function(req, res, next) {
+	db.collection('bugs').find().toArray(function(err, bugs) {
+	    db.collection('teams').findOne({"id": req.params.id}, function(err, team) {
+			res.render('dome', { title: 'Debug Competition', bugs: bugs, team: team });
+		});
+	});
 });
 
 module.exports = router;
