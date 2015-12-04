4e362f3aa6d1f68495f6f4a09ea45f0750b38615
Alexandre Terrasa <alexandre@moz-code.org>
2015-12-03 21:27:05 -0500

    wrapper: merge index and readme views
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- wrapper/routes/index.js
+++ wrapper/routes/index.js
@@ -9,15 +9,9 @@ var config = JSON.parse(fs.readFileSync(bug_dir + 'bug.json', 'UTF-8'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { config: config, title: 'Bug Challenge Wrapper'});
-});
-
-/* GET README page. */
-router.get('/readmes', function(req, res, next) {
   var public = md(fs.readFileSync(bug_dir + 'PUBLIC/README.md', 'UTF-8'));
   var private = md(fs.readFileSync(bug_dir + 'PRIVATE/README.md', 'UTF-8'));
-
-  res.render('readmes', { config: config, title: 'Readmes', public: public, private: private });
+  res.render('index', { config: config, title: 'Bug Wrapper', public: public, private: private });
 });
 
 function loadTestFiles(version, dir) {
