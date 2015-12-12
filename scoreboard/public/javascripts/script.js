fd6f4bbd085c32f38cf59376ae1df14c333168da
Alexandre Terrasa <alexandre@moz-code.org>
2015-12-12 01:23:29 -0500

    scoreboard: add timer on countown
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- scoreboard/public/javascripts/script.js
+++ scoreboard/public/javascripts/script.js
@@ -1,1 +1,17 @@
- $('[data-toggle="popover"]').popover();
+$(document).ready(function() {
+	var countdown = $('.countdown');
+	if(countdown) {
+		var ends = countdown.data('endsat');
+		var duration = countdown.data('duration');
+		setInterval(function() {
+			var rest = Math.floor((ends - new Date().getTime()) / 1000);
+			if(rest < 0) {
+				location.reload();
+			}
+			var value = Math.floor(rest * 100 / duration);
+			countdown.find('.progress-bar').attr('aria-valuenow', rest);
+			countdown.find('.progress-bar').css('width', value + '%');
+			countdown.find('.progress-bar').text(rest + 's');
+		}, 1000);
+	}
+});
