c2f27118bec7ccb5741020e09af193a424b36524
Alexandre Terrasa <alexandre@moz-code.org>
2015-12-12 01:41:17 -0500

    wrapper: cleanup
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- wrapper/routes/index.js
+++ wrapper/routes/index.js
@@ -1,3 +1,19 @@
+//  Copyright 2015 Alexandre Terrasa <alexandre@moz-code.org>.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+// Wrapper is responsible of building and testing all binaries of a team.
+
 var express = require('express');
 var fs = require('fs');
 var path = require('path');
@@ -56,7 +72,6 @@ function loadTestFiles(version, dir) {
 router.get('/', function(req, res, next) {
   var team = req.app.get('team');
   var bugs = listBugs(req.app.get('bugs_dir'));
-  console.log(bugs);
   res.render('team', { team: team, bugs: bugs });
 });
 
@@ -175,10 +190,8 @@ router.get('/bugs/:bug/check/:version', function(req, res, next) {
     if(err) {
 		res.json({error: err.message, resp: resp, body: body});
 	} else {
-		var response = {
-			bug: bug.name,
-			build: JSON.parse(body)
-		}
+		var response = bug.config;
+		response.build = JSON.parse(body);
 	    // Run check
 		if(response.build.status == "success") {
           var check_url = req.protocol + '://' + req.get('host') + '/bugs/' + bug.name + '/test/PUBLIC/';
