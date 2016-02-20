586da8b876ab5870b0d227c46d40248b0e6a5bab
Alexandre Terrasa <alexandre@moz-code.org>
2016-02-19 21:17:24 -0500

    scoreboard: autorefresh
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- src/public/javascripts/script.js
+++ src/public/javascripts/script.js
@@ -1,7 +1,19 @@
 $('[data-toggle="popover"]').popover();
 
-$(document).ready(function() {
+function refreshProgress(progressbar, value, label) {
+	progressbar.attr('aria-valuenow', value);
+	progressbar.css('width', value + '%');
+	progressbar.text(label);
+}
 
+function refreshBtn($panel, status) {
+	$btn = $panel.find("#" + status.bug);
+	$btn.removeClass("btn-success");
+	$btn.removeClass("btn-danger");
+	$btn.addClass(status.status == "success" ? "btn-success": "btn-danger");
+}
+
+$(function() {
 	$('.slot-icon').on('click', function() {
 		var bid = $(this).data('bid');
 		var tid = $(this).data('tid');
@@ -13,20 +25,60 @@ $(document).ready(function() {
 			$('[data-toggle="popover"]').popover();
 		});
 	});
+});
 
-	var countdown = $('.countdown');
-	if(countdown) {
-		var ends = countdown.data('endsat');
-		var duration = countdown.data('duration');
+$(function() {
+	setInterval(function() {
+		$('.panel-team-status').each(function(i, panel) {
+			var tid = $(panel).data('tid');
+			$.get('/json/team/' + tid, function(team) {
+				$panelStatus = $('.panel-team-status[data-tid=' + team.id + ']');
+				$panelStatus.find(".team-score").text(' ' + team.score + ' pts');
+				refreshProgress($panelStatus.find(".progress-bar-o2"), team.oxygen, team.oxygen + '%');
+				refreshProgress($panelStatus.find(".progress-bar-zz"), team.energy, team.energy + '%');
+				if(team.isDead) {
+					$panelStatus.removeClass('indanger');
+					$panelStatus.addClass('team isdead');
+				} else if(team.inDanger) {
+					$panelStatus.removeClass('isdead');
+					$panelStatus.addClass('team indanger');
+				} else {
+					$panelStatus.removeClass('indanger');
+					$panelStatus.removeClass('isdead');
+				}
+			});
+		});
+	}, 1000);
+
+	$('.panel-dome').each(function(i, panelDome) {
 		setInterval(function() {
-			var rest = Math.floor((ends - new Date().getTime()) / 1000);
-			if(rest < 0) {
-				location.reload();
-			}
-			var value = Math.floor(rest * 100 / duration);
-			countdown.find('.progress-bar').attr('aria-valuenow', rest);
-			countdown.find('.progress-bar').css('width', value + '%');
-			countdown.find('.progress-bar').text(rest + 's');
+			$panelDome = $(panelDome);
+			var tid = $panelDome.data('tid');
+			$.get('/json/status/' + tid, function(status) {
+				Object.keys(status.bugs).forEach(function(bug) {
+					refreshBtn($panelDome, status.bugs[bug]);
+				});
+			});
 		}, 1000);
-	}
+	});
+
+	$('.panel-round').each(function(i, panelRound) {
+		setInterval(function() {
+			$panelRound = $(panelRound);
+			$.get('/json/round/', function(round) {
+				var endsAt = round.startedAt + round.duration * 1000;
+				var rest = Math.floor(new Date(endsAt - new Date().getTime()) / 1000);
+				var value = Math.floor(rest * 100 / round.duration)
+				//refreshScore($panel, team.score)
+				if(round.gameFinished) {
+					$panelRound.find('h3').text('Competition is over');
+					$panelRound.find('div').remove();
+					$panelRound.append($('<div>').text('Please leave now.'))
+				} else {
+					$panelRound.find('h3').text('Round ' + round.round);
+					refreshProgress($panelRound.find(".progress-bar-round"), value, rest + 's');
+				}
+			});
+		}, 1000);
+	});
 });
---
+++ src/routes/json.js
@@ -0,0 +1,57 @@
+//  Copyright 2015 Alexandre Terrasa <alexandre@moz-code.org>.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+// JSON routes for ajax reload.
+
+var express = require('express');
+var request = require('request');
+var teams = require('../model/teams');
+var bugs = require('../model/bugs');
+var rounds = require('../model/rounds');
+var statuses = require('../model/status');
+var router = express.Router();
+
+router.get('/round', function(req, res, next) {
+	rounds.findLast(function(round) {
+		res.json(round);
+	});
+});
+
+router.get('/team/:tid', function(req, res, next) {
+	var tid = req.params.tid;
+	if(!tid) {
+		res.json({ error: "bad input" }, 503);
+		return;
+	}
+	teams.findOne(tid, function(team) {
+		if(!team) {
+			res.json({ error: "team not found" }, 404);
+			return;
+		}
+		res.json(team);
+	});
+});
+
+router.get('/status/:tid', function(req, res, next) {
+	var tid = req.params.tid;
+	if(!tid) {
+		res.json({ error: "bad input" }, 503);
+		return;
+	}
+	statuses.find({team: tid}, 1, function(status) {
+		res.json(status[0]);
+	});
+});
+
+module.exports = router;
