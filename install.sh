5384b2f28841ebc803da4ba528803aaa89756055
Alexandre Terrasa <alexandre@moz-code.org>
2016-02-25 03:42:22 -0500

    install: add dependencies
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+bfore= pwd
+
 apt-get install build-essential ccache libgc-dev graphviz libunwind-dev pkg-config
 
 apt-get install -y mongodb
 apt-get install -y nodejs
+apt-get install -y npm
 apt-get install -y openjdk-7-jdk
 apt-get install -y lua5.2
 apt-get install -y gcc
@@ -13,6 +16,7 @@ apt-get install -y mono-dmcs
 apt-get install -y ghc
 
 # update node js
+cp /usr/bin/nodejs /user/bin/node
 npm cache clean -f
 npm install -g n
 n stable
@@ -25,3 +29,8 @@ cd nit
 make
 source misc/nit_env.sh
 cd $bfore
+
+# install node deps
+cd src
+npm install
+cd ..
