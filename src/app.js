54fcea071a1b19cd0173a8d010269100e0709a45
Alexandre Terrasa <alexandre@moz-code.org>
2016-02-24 22:11:13 -0500

    routes: add admin route
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var json = require('./routes/json');
+var admin = require('./routes/admin');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/json', json);
+app.use('/admin', admin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
