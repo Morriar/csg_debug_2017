6db4992151737e92bb5e9c5f044ee321406c0a1d
Alexandre Terrasa <alexandre@moz-code.org>
2016-02-27 03:48:03 -0500

    lore: add copyrights on test files
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- bugs/power_grid/PUBLIC/tests.sh
+++ bugs/power_grid/PUBLIC/tests.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Copyright Dome Systems.
+#
+# Dome Private License (D-PL) [a369] PubPL 36 (7 Gallium 369)
+#
+# * URL: http://csgames.org/2016/dome_license.md
+# * Type: Software
+# * Media: Software
+# * Origin: Mines of Morriar
+# * Author: Morriar
+
 compile()
 {
 	compile_log=$1
