3805509edb08f61cfe7cf1b2e2f2ed25e5bd0315
Alexandre Terrasa <alexandre@moz-code.org>
2016-02-28 20:02:23 -0500

    bugs: jail bug_hello
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- bugs/bug_hello/PRIVATE/tests.sh
+++ bugs/bug_hello/PRIVATE/tests.sh
@@ -25,8 +25,20 @@ run_test()
 {
 	run_input=$1
 	run_output=$2
-	timeout -k 3 3 bin/hello $(cat $run_input) > "$run_output" 2>&1
+	jail_home=$(dirname $run_output)
+	make_jail $jail_home bin/hello $run_input
+	timeout -k 3 3 firejail --profile=jail.profile --quiet --private=$jail_home ./hello < "$run_input" 2>&1 | grep -v "Reading profile" > "$run_output"
 	return $?
+}
+
+make_jail()
+{
+	jail_dir=$1
+	jail_bin=$2
+	jail_input=$3
+	mkdir -p $jail_dir
+	cp $jail_bin $jail_dir
+	cp $jail_input $jail_dir
 }
 
 diff_test()
