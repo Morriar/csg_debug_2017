fbd9fcee04e3220937c61ce8afbec71f323dc9ef
Alexandre Terrasa <alexandre@moz-code.org>
2016-02-28 20:54:59 -0500

    bugs: jail power_grid
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- bugs/power_grid/PRIVATE/tests.sh
+++ bugs/power_grid/PRIVATE/tests.sh
@@ -25,8 +25,20 @@ run_test()
 {
 	run_input=$1
 	run_output=$2
-	timeout -k 3 3 bin/z_status "$run_input" > "$run_output" 2>&1
+	jail_home=$(dirname $run_output)
+	make_jail $jail_home bin/z_status $run_input
+	timeout -k 3 3 firejail --profile=jail.profile --quiet --private=$jail_home ./z_status "$run_input" 2>&1 | grep -v "Reading profile" > "$run_output"
 	return $?
+}
+
+make_jail()
+{
+	jail_dir=$1
+	jail_bin=$2
+	jail_input=$3
+	mkdir -p $jail_dir/tests
+	cp $jail_bin $jail_dir
+	cp $jail_input $jail_dir/tests/.
 }
 
 diff_test()
