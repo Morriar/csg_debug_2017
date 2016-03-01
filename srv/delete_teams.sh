ae3271c89b330206da1b3758ff9cbc36651fde80
Alexandre Terrasa <alexandre@moz-code.org>
2016-03-01 01:00:49 -0500

    srv: fix delete team script
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- srv/delete_teams.sh
+++ srv/delete_teams.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+	echo "usage: delete_teams teams.csv"
+	exit 1
+fi
+
 while IFS=$'\t' read -r -a user
 do
 	userdel "${user[0]}"
 	rm -rf /home/"${user[0]}"
-done < teams.csv
+done < $1
