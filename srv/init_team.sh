f736ddd4bbd61f666d614fb078ffa51f5c670d51
Alexandre Terrasa <alexandre@moz-code.org>
2016-03-04 19:09:05 -0500

    tmp: use 9291 port for git
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- srv/init_team.sh
+++ srv/init_team.sh
@@ -14,6 +14,7 @@ chmod 600 ~/.ssh/id_rsa_debug
 echo "# Debug" >> ~/.ssh/config
 echo "Host debug" >> ~/.ssh/config
 echo "HostName $teamip" >> ~/.ssh/config
+echo "Port 9291" >> ~/.ssh/config
 echo "User $teamid" >> ~/.ssh/config
 echo "IdentityFile ~/.ssh/id_rsa_debug" >> ~/.ssh/config
 source ~/.profile
@@ -23,16 +24,16 @@ ssh-add ~/.ssh/id_rsa_debug
 mkdir -p $teamid
 cd $teamid
 
-git clone ssh://debug:/home/$teamid/access_panel
-git clone ssh://debug:/home/$teamid/alu_sim
-git clone ssh://debug:/home/$teamid/atomic_engine
-git clone ssh://debug:/home/$teamid/bug_hello
-git clone ssh://debug:/home/$teamid/domesec
-git clone ssh://debug:/home/$teamid/doors
-git clone ssh://debug:/home/$teamid/irrigations
-git clone ssh://debug:/home/$teamid/power_grid
-git clone ssh://debug:/home/$teamid/militia_dispatch
-git clone ssh://debug:/home/$teamid/shield_gen
-git clone ssh://debug:/home/$teamid/thermal_reactor
-git clone ssh://debug:/home/$teamid/ventilation
-git clone ssh://debug:/home/$teamid/water_supply
+git clone ssh://debug/home/$teamid/access_panel
+git clone ssh://debug/home/$teamid/alu_sim
+git clone ssh://debug/home/$teamid/atomic_engine
+git clone ssh://debug/home/$teamid/bug_hello
+git clone ssh://debug/home/$teamid/domesec
+git clone ssh://debug/home/$teamid/doors
+git clone ssh://debug/home/$teamid/irrigations
+git clone ssh://debug/home/$teamid/power_grid
+git clone ssh://debug/home/$teamid/militia_dispatch
+git clone ssh://debug/home/$teamid/shield_gen
+git clone ssh://debug/home/$teamid/thermal_reactor
+git clone ssh://debug/home/$teamid/ventilation
+git clone ssh://debug/home/$teamid/water_supply
