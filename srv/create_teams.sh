d4ce67b2600bd5eb9a1945d719d6df2621f5e5ce
Alexandre Terrasa <alexandre@moz-code.org>
2016-03-07 23:28:43 -0500

    srv: rename directory `init` in `debug`
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- srv/create_teams.sh
+++ srv/create_teams.sh
@@ -16,13 +16,13 @@ create_user() {
 	chgrp $userid /home/$userid/.ssh/authorized_keys
 
 	# create key repo
-	echo $userpw | sudo -s -u $userid mkdir /home/$userid/init
-	cp /home/ubuntu/csg_debug/srv/init_team.sh /home/$userid/init/
-	echo $userpw | sudo -s -u $userid cp /home/$userid/.ssh/id_rsa /home/$userid/init/id_rsa_debug
-	echo $userpw | sudo -s -u $userid git --git-dir /home/$userid/init init --bare
-	echo $userpw | sudo -s -u $userid git --git-dir /home/$userid/init --work-tree /home/$userid/init add /home/$userid/init/init_team.sh
-	echo $userpw | sudo -s -u $userid git --git-dir /home/$userid/init --work-tree /home/$userid/init add /home/$userid/init/id_rsa_debug
-	echo $userpw | sudo -s -u $userid git --git-dir /home/$userid/init --work-tree /home/$userid/init commit -m "Init repo"
+	echo $userpw | sudo -s -u $userid mkdir /home/$userid/debug
+	cp /home/ubuntu/csg_debug/srv/clone_repos.sh /home/$userid/debug/
+	echo $userpw | sudo -s -u $userid cp /home/$userid/.ssh/id_rsa /home/$userid/debug/id_rsa_debug
+	echo $userpw | sudo -s -u $userid git --git-dir /home/$userid/debug init --bare
+	echo $userpw | sudo -s -u $userid git --git-dir /home/$userid/debug --work-tree /home/$userid/debug add /home/$userid/debug/clone_repos.sh
+	echo $userpw | sudo -s -u $userid git --git-dir /home/$userid/debug --work-tree /home/$userid/debug add /home/$userid/debug/id_rsa_debug
+	echo $userpw | sudo -s -u $userid git --git-dir /home/$userid/debug --work-tree /home/$userid/debug commit -m "Init repo"
 
 	# protect all files in repo
 	chmod -R 770 /home/$userid
