cb46136861371917e3f7ca5962ad616f8e14f61e
Alexandre Terrasa <alexandre@moz-code.org>
2017-03-23 17:52:37 -0400

    bugs: add jail for OUIJA
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- bugs/ouija/PRIVATE/tests.sh
+++ bugs/ouija/PRIVATE/tests.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-#                 UQAM ON STRIKE PUBLIC LICENSE
-#                    Version 2, December 2004
+# Copyright 2017 Alexandre Terrasa <alexandre@moz-code.org>.
 #
-# Copyright (C) 2017
-# Alexandre Terrasa <@>,
-# Philippe Pepos Petitclerc <@>
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Everyone is permitted to copy and distribute verbatim or modified
-# copies of this license document, and changing it is allowed as long
-# as the name is changed.
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
-#                 UQAM ON STRIKE PUBLIC LICENSE
-#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-#
-#  0. You just do what the fuck you want to as long as you're on strike.
-#
-# aHR0cDovL2NzZ2FtZXMub3JnLzIwMTYvCg==
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 compile()
 {
@@ -29,8 +25,21 @@ run_test()
 {
 	run_input=$1
 	run_output=$2
-	bin/ouija "$run_input" | strings -e S > "$run_output" 2>&1
+
+	jail_home=$(dirname $run_output)
+	make_jail $jail_home bin/ $run_input
+	timeout -k 3 3 firejail --profile=jail.profile --quiet --private=$jail_home bin/ouija "`basename $run_input`" 2>&1 | strings -e S | grep -v "Reading profile" > "$run_output"
 	return $?
+}
+
+make_jail()
+{
+	jail_dir=$1
+	jail_bin=$2
+	jail_input=$3
+	mkdir -p $jail_dir
+	cp -r $jail_bin $jail_dir
+	cp -r $jail_input $jail_dir
 }
 
 diff_test()
@@ -46,12 +55,12 @@ run_all()
 {
 	out_dir=$1
 	tests_dir=$2
-	rm -rf $out_dir/*
+	timestamp=$(date +"%s")
 
 	echo "Compile bin..."
-	compile "$out_dir/compile.log"
+	compile "$out_dir/compile.$timestamp.log"
 	if [ "$?" -ne 0 ]; then
-		echo -e " * [\e[31mFAIL\e[0m] compile bin (cat $out_dir/compile.log)"
+		echo -e " * [\e[31mFAIL\e[0m] compile bin (cat $out_dir/compile.$timestamp.log)"
 		exit 1
 	fi
 
@@ -66,11 +75,11 @@ run_all()
 		file=$(basename "$input")
 		name="${file%.*}"
 
-		out="$out_dir/$name.out"
+		out="$out_dir/$name.$timestamp.out"
 		exp="$tests_dir/$name.res"
 
 		run_test "$input" "$out"
-		diff_test "$out" "$exp" "$out_dir/$name.diff"
+		diff_test "$out" "$exp" "$out_dir/$name.$timestamp.diff"
 		if [ "$?" == 0 ]; then
 			echo -e " * [\e[32mOK\e[0m] $name"
 			OK=$(($OK + 1))
@@ -95,28 +104,29 @@ run_one()
 	name=$1
 	out_dir=$2
 	tests_dir=$3
+	timestamp=$(date +"%s")
 
 	echo "Compile bin..."
-	compile "$out_dir/compile.log"
+	compile "$out_dir/compile.$timestamp.log"
 	if [ "$?" -ne 0 ]; then
 		echo -e "[\e[31mBUILD FAIL\e[0m]"
-		cat "$out_dir/compile.log"
+		cat "$out_dir/compile.$timestamp.log"
 		return 1
 	fi
 
 	input="$tests_dir/$name.in"
-	out="$out_dir/$name.out"
+	out="$out_dir/$name.$timestamp.out"
 	exp="$tests_dir/$name.res"
 
 	echo "Run tests..."
 	run_test "$input" "$out"
-	diff_test "$out" "$exp" "$out_dir/$name.diff"
+	diff_test "$out" "$exp" "$out_dir/$name.$timestamp.diff"
 	if [ "$?" == 0 ]; then
 		echo -e "[\e[32mOK\e[0m] $name"
 		return 0
 	else
 		echo -e "[\e[31mFAIL\e[0m] $name"
-		cat "$out_dir/$name.diff"
+		cat "$out_dir/$name.$timestamp.diff"
 		return 1
 	fi
 }
