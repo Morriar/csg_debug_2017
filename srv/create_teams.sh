763d41484d8fdc896b4f248bd1dd307a7e65da6b
Alexandre Terrasa <alexandre@moz-code.org>
2017-03-23 21:18:37 -0400

    deploy: update scripts for new architecture
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- srv/create_teams.sh
+++ srv/create_teams.sh
@@ -2,39 +2,37 @@
 
 create_user() {
 	userid=$1
-	userpw=$2
-	cryptd=$(mkpasswd "$userpw")
+	keys_dir=$2
+	cryptd=$(mkpasswd "pwd$userid")
 
 	# create user
-	# useradd -m -p "$cryptd" "$userid"
 	useradd -s /usr/bin/git-shell -m -p "$cryptd" "$userid"
 
-	# gen rsa key
-	echo $userpw | sudo -s -u $userid ssh-keygen -C $userid -t rsa -f /home/$userid/.ssh/id_rsa -N $userpw
-	echo $userpw | sudo -s -u $userid echo $(cat /home/$userid/.ssh/id_rsa.pub) >> /home/$userid/.ssh/authorized_keys
+	# add pub key
+	cat keys_dir/__admin.key.pub >> /home/$userid/.ssh/authorized_keys # admins keys
+	cat keys_dir/$userid.key.pub >> /home/$userid/.ssh/authorized_keys
 	chown $userid /home/$userid/.ssh/authorized_keys
 	chgrp $userid /home/$userid/.ssh/authorized_keys
 
 	# create key repo
-	echo $userpw | sudo -s -u $userid mkdir /home/$userid/debug
+	mkdir /home/$userid/debug
 	cp /home/ubuntu/csg_debug/srv/clone_repos.sh /home/$userid/debug/
-	echo $userpw | sudo -s -u $userid cp /home/$userid/.ssh/id_rsa /home/$userid/debug/id_rsa_debug
-	echo $userpw | sudo -s -u $userid git --git-dir /home/$userid/debug init --bare
-	echo $userpw | sudo -s -u $userid git --git-dir /home/$userid/debug --work-tree /home/$userid/debug add /home/$userid/debug/clone_repos.sh
-	echo $userpw | sudo -s -u $userid git --git-dir /home/$userid/debug --work-tree /home/$userid/debug add /home/$userid/debug/id_rsa_debug
-	echo $userpw | sudo -s -u $userid git --git-dir /home/$userid/debug --work-tree /home/$userid/debug commit -m "Init repo"
+	git --git-dir /home/$userid/debug init --bare
+	git --git-dir /home/$userid/debug --work-tree /home/$userid/debug add /home/$userid/debug/clone_repos.sh
+	git --git-dir /home/$userid/debug --work-tree /home/$userid/debug commit -m "Init repo"
 
 	# protect all files in repo
 	chmod -R 770 /home/$userid
+	chown -R $userid /home/$userid/debug
+	chgrp -R $userid /home/$userid/debug
 }
 
-
-if [ "$#" -ne 1 ]; then
-	echo "usage: create_teams teams.csv"
+if [ "$#" -ne 2 ]; then
+	echo "usage: create_teams teams.lst keys_dir/"
 	exit 1
 fi
 
 while IFS=$'\t' read -r -a user
 do
-	create_user "${user[0]}" "${user[1]}"
+	create_user "${user[0]}" $2
 done < $1
--- srv/init_teams.sh
+++ srv/init_teams.sh
@@ -8,17 +8,18 @@ fi
 while IFS=$'\t' read -r -a user
 do
 	team=${user[0]}
-	pass=${user[1]}
 
 	team_dir=$2/$team
 
-	for bug in access_panel alveoli alu_sim atomic_engine bug_hello domesec domefs doors irrigations militia_dispatch power_grid shield_gen thermal_reactor ventilation water_supply; do
+	for bug in bug_hello CalendarPersonalPlanner Filther FireMatches ouija PearMap PegMobile ROMCryption solo SuperSecretSafeSystemSolution Wrottit; do
 		bug_origin=$team_dir/${bug}_origin
 		# Set permissions
 		chown -R $team $bug_origin;
 		chgrp -R $team $bug_origin;
 		# Create home link
-		echo $pass | sudo -s -u $team ln -fs $bug_origin /home/$team/$bug
+		ln -fs $bug_origin /home/$team/$bug
+		chown -R $team /home/$team/$bug;
+		chgrp -R $team /home/$team/$bug;
 	done
 done < $1
 chmod -R 751 $2;
