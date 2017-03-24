cbaa843c2b9609aeeda8418fb83ab5e3239bc601
Alexandre Terrasa <alexandre@moz-code.org>
2017-03-23 23:44:10 -0400

    bugs: break Filther for ever...
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- bugs/Filther/PUBLIC/src/filther.pl
+++ bugs/Filther/PUBLIC/src/filther.pl
@@ -17,5 +17,5 @@
 #
 # aHR0cDovL2NzZ2FtZXMub3JnLzIwMTYvCg==
 
-use Switch;
+#use Switch;
 $_=<>;chomp;switch($_){case('twitter'){$/=undef;$_=<>;m/.{140}/s;print$&;print"\n";}case('revers'){while(<>){chomp;print scalar reverse,"\n";}}case('#hashtag'){while(<>){while(/([A-za-z0-9]{4,})/g){$c{"#$1"}++;}}print(join"\x20",((sort{$c{$b}cmp$c{$a}}sort(keys%c))[0..9]),"\n");}case('kikoolol'){while(<>){s/(.)(.)/uc$1.lc$2/eg;print;}}else{print"Cannot:$_";}}
