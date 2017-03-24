9081f6f58000109e83707e0cc7c90022c5897f27
Your Name <you@example.com>
2017-03-24 02:07:54 +0000

    deploy: fix deplot teams scripts

--- srv/create_teams.sh
+++ srv/create_teams.sh
@@ -3,28 +3,26 @@
 create_user() {
 	userid=$1
 	keys_dir=$2
-	cryptd=$(mkpasswd "pwd$userid")
 
 	# create user
-	useradd -s /usr/bin/git-shell -m -p "$cryptd" "$userid"
+	useradd -s /usr/bin/git-shell -m -p "PASSword" "$userid"
 
 	# add pub key
-	cat keys_dir/__admin.key.pub >> /home/$userid/.ssh/authorized_keys # admins keys
-	cat keys_dir/$userid.key.pub >> /home/$userid/.ssh/authorized_keys
-	chown $userid /home/$userid/.ssh/authorized_keys
-	chgrp $userid /home/$userid/.ssh/authorized_keys
+	mkdir /home/$userid/.ssh
+	cat $keys_dir/__admin.key.pub >> /home/$userid/.ssh/authorized_keys # admins keys
+	cat $keys_dir/$userid.key.pub >> /home/$userid/.ssh/authorized_keys
 
 	# create key repo
 	mkdir /home/$userid/debug
-	cp /home/ubuntu/csg_debug/srv/clone_repos.sh /home/$userid/debug/
+	cp srv/clone_repos.sh /home/$userid/debug/
 	git --git-dir /home/$userid/debug init --bare
 	git --git-dir /home/$userid/debug --work-tree /home/$userid/debug add /home/$userid/debug/clone_repos.sh
 	git --git-dir /home/$userid/debug --work-tree /home/$userid/debug commit -m "Init repo"
 
 	# protect all files in repo
 	chmod -R 770 /home/$userid
-	chown -R $userid /home/$userid/debug
-	chgrp -R $userid /home/$userid/debug
+	chown -R $userid /home/$userid
+	chgrp -R $userid /home/$userid
 }
 
 if [ "$#" -ne 2 ]; then
