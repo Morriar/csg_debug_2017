e4840b68c06d0519960e219d8522719ca250f999
Alexandre Terrasa <alexandre@moz-code.org>
2017-03-25 20:17:17 -0400

    srv: update clone_repos tool for new architecture
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- srv/clone_repos.sh
+++ srv/clone_repos.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-	echo "usage: clone_repos IP TEAM"
+if [ "$#" -ne 1 ]; then
+	echo "usage: clone_repos TEAM"
 	exit 1
 fi
 
-ip=$1
 teamid=$2
 
-git clone ssh://$teamid@$ip:/home/$teamid/bug_hello
-git clone ssh://$teamid@$ip:/home/$teamid/CalendarPersonalPlanner
-git clone ssh://$teamid@$ip:/home/$teamid/Filther
-git clone ssh://$teamid@$ip:/home/$teamid/FireMatches
-git clone ssh://$teamid@$ip:/home/$teamid/ouija
-git clone ssh://$teamid@$ip:/home/$teamid/PearMap
-git clone ssh://$teamid@$ip:/home/$teamid/PegMobile
-git clone ssh://$teamid@$ip:/home/$teamid/ROMCryption
-git clone ssh://$teamid@$ip:/home/$teamid/solo
-git clone ssh://$teamid@$ip:/home/$teamid/SuperSecretSafeSystemSolution
-git clone ssh://$teamid@$ip:/home/$teamid/Wrottit
+git config --global user.email "$teamid"
+git config --global user.name "$teamid"
+
+git clone ssh://$teamid@debug.csgames:/home/$teamid/bug_hello
+git clone ssh://$teamid@debug.csgames:/home/$teamid/CalendarPersonalPlanner
+git clone ssh://$teamid@debug.csgames:/home/$teamid/Filther
+git clone ssh://$teamid@debug.csgames:/home/$teamid/FireMatches
+git clone ssh://$teamid@debug.csgames:/home/$teamid/ouija
+git clone ssh://$teamid@debug.csgames:/home/$teamid/PearMap
+git clone ssh://$teamid@debug.csgames:/home/$teamid/PegMobile
+git clone ssh://$teamid@debug.csgames:/home/$teamid/ROMCryption
+git clone ssh://$teamid@debug.csgames:/home/$teamid/solo
+git clone ssh://$teamid@debug.csgames:/home/$teamid/SuperSecretSafeSystemSolution
+git clone ssh://$teamid@debug.csgames:/home/$teamid/Wrottit
